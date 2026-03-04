6459522f740491ff05040b1a3eb88ed1bc9e903a
Niklas Freund <nalquas.dev@gmail.com>
2026-03-04 13:23:50 +0100

    add auto-ssh-agent.sh

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # auto-ssh-agent
-Shell startup script for automatically starting and connecting ssh-agent
+
+Shell startup script for automatically starting and connecting ssh-agent. To add ssh keys to the agent until reboot or the configured 10 hour timeout, use `ssh-add`.
+
+## Installation
+
+### Bash
+
+Add the following to your `.bashrc` file (and adjust the path):
+
+```sh
+# Activate auto-ssh-agent
+. /path/to/auto-ssh-agent/auto-ssh-agent.sh
+```
+
+### Zsh
+
+Add the following to your `.zshrc` file (and adjust the path):
+
+```sh
+# Activate auto-ssh-agent
+source /path/to/auto-ssh-agent/auto-ssh-agent.sh
+```
---
+++ auto-ssh-agent.sh
@@ -0,0 +1,26 @@
+# NOTE: no shebang added on purpose because this may be imported into different shells, like bash or zsh
+
+# Create a helper function for quickly setting up ssh-agent when needed
+auto-ssh-agent() {
+    echo "Preparing ssh-agent ..."
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+        echo "There is no ssh-agent running, starting a new one ..."
+        ssh-agent -t 10h > "$XDG_RUNTIME_DIR/.ssh-agent.env"
+        source "$XDG_RUNTIME_DIR/.ssh-agent.env" > /dev/null
+    elif [[ ! "$SSH_AGENT_PID" ]]; then
+        if [ -f "$XDG_RUNTIME_DIR/.ssh-agent.env" ]; then
+            echo "There already is an ssh-agent running, connecting to it ..."
+            source "$XDG_RUNTIME_DIR/.ssh-agent.env" > /dev/null
+        else
+            # HACK: workaround needed to work on Ubuntu, which likes starting its own ssh-agent that we can't use easily
+            ssh-agent -t 10h > "$XDG_RUNTIME_DIR/.ssh-agent.env"
+            source "$XDG_RUNTIME_DIR/.ssh-agent.env" > /dev/null
+        fi
+    else
+        echo "Already connected to an ssh-agent!"
+    fi
+    echo "... done!"
+}
+
+# Load SSH stuff
+auto-ssh-agent
