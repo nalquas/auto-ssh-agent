fa980fa1a53c2f9024fffd1d0794007541f4a2e6
Niklas Freund <nalquas.dev@gmail.com>
2026-03-26 16:36:41 +0100

    add ssh-agent lifetime setting

--- README.md
+++ README.md
@@ -1,23 +1,15 @@
 # auto-ssh-agent
 
-Shell startup script for automatically starting and connecting ssh-agent. To add ssh keys to the agent until reboot or the configured 10 hour timeout, use `ssh-add`.
+Shell startup script for automatically starting and connecting ssh-agent. To add ssh keys to the agent until reboot or the configured timeout, use `ssh-add`.
 
 ## Installation
 
-### Bash
-
-Add the following to your `.bashrc` file (and adjust the path):
-
-```sh
-# Activate auto-ssh-agent
-. /path/to/auto-ssh-agent/auto-ssh-agent.sh
-```
-
-### Zsh
-
-Add the following to your `.zshrc` file (and adjust the path):
+Add the following to your `.bashrc` or `.zshrc` file (and adjust the path):
 
 ```sh
 # Activate auto-ssh-agent
 source /path/to/auto-ssh-agent/auto-ssh-agent.sh
+auto-ssh-agent 10h
 ```
+
+You can vary the lifetime of the ssh-agent as wanted, e.g. `2h`. If no lifetime is given, it defaults to `10h`.
--- auto-ssh-agent.sh
+++ auto-ssh-agent.sh
@@ -5,11 +5,12 @@ _auto-ssh-agent-load-agent() {
 }
 
 _auto-ssh-agent-create-new-agent() {
-	ssh-agent -t 10h >"$XDG_RUNTIME_DIR/.ssh-agent.env"
+	ssh-agent -t "${1:-10h}" >"$XDG_RUNTIME_DIR/.ssh-agent.env"
 }
 
 auto-ssh-agent() {
 	echo -n "Preparing ssh-agent ... "
+	lifetime="${1:-10h}"
 
 	if [ -z "${XDG_RUNTIME_DIR:-}" ]; then
 		echo "XDG_RUNTIME_DIR is not set, cannot manage ssh-agent environment file!"
@@ -18,15 +19,13 @@ auto-ssh-agent() {
 
 	if ! pgrep -u "$USER" ssh-agent >/dev/null; then
 		echo -n "no ssh-agent running, starting a new one ... "
-		_auto-ssh-agent-create-new-agent
+		_auto-ssh-agent-create-new-agent "$lifetime"
 	elif [ ! -f "$XDG_RUNTIME_DIR/.ssh-agent.env" ]; then
 		# HACK: workaround needed to work on Ubuntu, which likes starting its own ssh-agent that we can't use easily
 		echo -n "inaccessible ssh-agent running, starting a new one ... "
-		_auto-ssh-agent-create-new-agent
+		_auto-ssh-agent-create-new-agent "$lifetime"
 	fi
 	echo -n "connecting ... "
 	_auto-ssh-agent-load-agent
 	echo "done!"
 }
-
-auto-ssh-agent
